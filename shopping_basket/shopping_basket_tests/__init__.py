34e72f58c6e0d79f425f4aa21889cd65171acc33
gkyrtsidis <gkyrtsidis@crfs.com>
2021-10-16 10:35:14 +0100

    Add assumptions comments specifically to share my thoughts in regard to this assignment. Not a normal practise.

--- shopping_basket/shopping_basket_tests/__init__.py
+++ shopping_basket/shopping_basket_tests/__init__.py
@@ -4,6 +4,11 @@ from shopping_basket.basket_pricer import Catalogue
 
 class CatalogueTests(unittest.TestCase):
     def test_catalogue_add(self):
+        # Assumptions:
+        # We assume negative prices are accepted
+        # Rounding up/down is being done to the closest
+        # Add can behave like an update function
+        # Invalid values will raise ValueError and thus, not accepted
         catalogue = Catalogue()
 
         catalogue.add("test", 5)
@@ -24,12 +29,17 @@ class CatalogueTests(unittest.TestCase):
         catalogue.add("test", 6.5)
         self.assertEqual(catalogue.catalogue["test"], 6.5)
 
+        catalogue.add("test", -5)
+        self.assertEqual(catalogue.catalogue["test"], -5)
+
         self.assertRaises(ValueError, catalogue.add, "test", "asdf")
         self.assertRaises(ValueError, catalogue.add, "test", "£7")
         self.assertRaises(ValueError, catalogue.add, "test", "8")
 
     @staticmethod
     def test_catalogue_remove():
+        # Assumptions:
+        # If key is wrong then do nothing (we could've raised an exception instead)
         catalogue = Catalogue()
         catalogue.add("test", 5)
         catalogue.remove("test")
@@ -39,6 +49,9 @@ class CatalogueTests(unittest.TestCase):
         catalogue.remove(0)
 
     def test_catalogue_generic(self):
+        # Assumptions:
+        # Invalid usage/access, will default to KeyError exception
+        # We want to limit unexpected behaviour/usage but there is a diminishing returns to the value added vs dev time
         catalogue = Catalogue()
         self.assertRaises(KeyError, lambda: catalogue.catalogue["test"])
 
