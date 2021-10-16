004bbbd408d0c6ea7d0081fc180789ae4abd842a
gkyrtsidis <gkyrtsidis@crfs.com>
2021-10-16 19:18:16 +0100

    Add BasketTests and refactor remove calls to throw ValueError exception when the item provided to be removed is not found.

--- shopping_basket/shopping_basket_tests/__init__.py
+++ shopping_basket/shopping_basket_tests/__init__.py
@@ -1,5 +1,5 @@
 import unittest
-from shopping_basket.basket_pricer import Catalogue
+from shopping_basket.basket_pricer import Catalogue, Offers, Basket
 
 
 class CatalogueTests(unittest.TestCase):
@@ -32,28 +32,25 @@ class CatalogueTests(unittest.TestCase):
         catalogue.add("test", -5)
         self.assertEqual(catalogue.catalogue["test"], -5)
 
-        self.assertRaises(ValueError, catalogue.add, "test", "asdf")
+        self.assertRaises(ValueError, catalogue.add, "test", "Lorem ipsum")
         self.assertRaises(ValueError, catalogue.add, "test", "£7")
         self.assertRaises(ValueError, catalogue.add, "test", "8")
 
-    @staticmethod
-    def test_remove():
+    def test_remove(self):
         # Assumptions:
-        # If key is wrong then do nothing (we could've raised an exception instead)
+        # If key is wrong then raise ValueError exception
+        # Invalid access, will default to KeyError exception
         catalogue = Catalogue()
         catalogue.add("test", 5)
+        self.assertEqual(catalogue.catalogue["test"], 5)
         catalogue.remove("test")
-        catalogue.remove("test")
-        catalogue.remove("asdf")
-        catalogue.add("test", 5)
-        catalogue.remove(0)
-
-    def test_generic(self):
-        # Assumptions:
-        # Invalid usage/access, will default to KeyError exception
-        # We want to limit unexpected behaviour/usage but there is a diminishing returns to the value added vs dev time
-        catalogue = Catalogue()
         self.assertRaises(KeyError, lambda: catalogue.catalogue["test"])
+        self.assertRaises(ValueError, catalogue.remove, "test")
+        self.assertRaises(ValueError, catalogue.remove, "Lorem ipsum")
+        catalogue.add("test", 5)
+        self.assertEqual(catalogue.catalogue["test"], 5)
+        self.assertRaises(ValueError, catalogue.remove, 0)
+        self.assertEqual(catalogue.catalogue["test"], 5)
 
 
 class OffersTests(unittest.TestCase):
@@ -61,7 +58,56 @@ class OffersTests(unittest.TestCase):
 
 
 class BasketTests(unittest.TestCase):
-    pass
+    def test_add(self):
+        # Assumptions:
+        # Add can add news elements
+        # Add can update existing elements (like an update function) for simplicity reasons
+        # Add will not increment the quantity, it will instead replace the previous value
+        # Negative or 0 values in quantity will remove the element
+        # Unexpected values will throw ValueError exception
+        # We verify the product exists in the catalogue before adding it to the basket
+        catalogue = Catalogue()
+        catalogue.add("Baked Beans", 3.26)
+        basket = Basket(catalogue)
+
+        basket.add("Baked Beans", 4)
+
+        self.assertRaises(ValueError, basket.add, "Biscuits", 1)
+
+        basket.add("Baked Beans", 2)
+        self.assertEqual(basket.basket["Baked Beans"], 2)
+
+        basket.add("Baked Beans", 0)
+        self.assertRaises(KeyError, lambda: basket.basket["Baked Beans"])
+
+        self.assertRaises(ValueError, basket.add, "Baked Beans", -15)
+        self.assertRaises(KeyError, lambda: basket.basket["Baked Beans"])
+
+        self.assertRaises(ValueError, basket.add, "Baked Beans", "£1")
+        self.assertRaises(ValueError, basket.add, "Baked Beans", "Lorem ipsum")
+        self.assertRaises(ValueError, basket.add, "Lorem ipsum", 100)
+
+    def test_remove(self):
+        # Assumptions:
+        # If key is wrong then raise ValueError exception
+        # Invalid access, will default to KeyError exception
+        catalogue = Catalogue()
+        catalogue.add("test", 3.26)
+        basket = Basket(catalogue)
+
+        basket.add("test", 5)
+        self.assertEqual(basket.basket["test"], 5)
+
+        basket.remove("test")
+        self.assertRaises(KeyError, lambda: basket.basket["test"])
+
+        self.assertRaises(ValueError, basket.remove, "test")
+        self.assertRaises(ValueError, basket.remove, "Lorem ipsum")
+        self.assertRaises(ValueError, basket.remove, 0)
+
+        basket.add("test", 5)
+        self.assertEqual(basket.basket["test"], 5)
+        self.assertEqual(basket.basket["test"], 5)
 
 
 class BasketPricerTests(unittest.TestCase):
